f49d65fcb38ecf67c52f615d7aa196a41a80de7a
Kolusu Dhana Sri <dhanasrikolusu@gmail.com>
2026-04-21 13:05:16 +0530

    updated ML URL in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'http://localhost:5000/api';
-const ML_URL = 'http://localhost:5001'; // Flask service port
+const ML_URL = 'https://houseprice-prediction-ej1n.onrender.com';// Flask service port
 
 const api = axios.create({ baseURL: API_URL });
 
